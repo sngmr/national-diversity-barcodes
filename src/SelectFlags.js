79a98e54a510a707e9a84d48c4fef2c44d0b76a4
Shingo Mori <shingo-mori@azapa.co.jp>
2021-06-10 21:56:46 +0900

    Implements select and deselect countries on the table

--- src/SelectFlags.js
+++ src/SelectFlags.js
@@ -3,36 +3,64 @@ import FlagsData from "./FlagsData";
 
 function SelectFlags() {
     const [flagsData, setFlagsData] = useState([]);
+    const [selectedFlagsData, setSelectedFlagsData] = useState([]);
     
     // When page load complete
     useEffect(() => {
         FlagsData.getAllNationalFlags().then((data) => {
-            console.log(data.data[0]);
             setFlagsData(data.data);
         });
     }, []);
     
+    // When selected flags
+    useEffect(() => {
+        console.log(selectedFlagsData);
+    }, [selectedFlagsData]);
+    
+    // Create TR country node
+    const createCountryTr = (flagData) => {
+        // When countries table selected
+        const onCountryRowClick = () => {
+            if (selectedFlagsData.indexOf(flagData) >= 0) {
+                // Remove
+                setSelectedFlagsData(selectedFlagsData.filter(v => flagData !== v));
+            } else {
+                // Add
+                setSelectedFlagsData([...selectedFlagsData, flagData]);
+            }
+        }
+        
+        // Check if this country is in selected list
+        let isSelected = false;
+        if (selectedFlagsData.indexOf(flagData) >= 0) {
+            isSelected = true;
+        }
+    
+        return (
+            <tr key={flagData.alpha3Code}
+                onClick={() => { onCountryRowClick(); }}
+                className={isSelected ? 'is-selected' : ''}
+            >
+                <td>{flagData.name}</td>
+            </tr>
+        );
+    }
+    
     return (
         <div className="columns">
             <div className="column">
-                <strong>Select name(s)</strong>
+                <strong>Select countries</strong>
                 <div className="table-container country-table-container">
                     <table className="table is-bordered is-striped is-narrow is-hoverable">
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                        </tr>
-                        </thead>
-        
                         <tbody>
-                        {flagsData.map((flagData) =>
-                            <tr key={flagData.alpha3Code}>
-                                <td>{flagData.name}</td>
-                            </tr>
-                        )}
+                        {flagsData.map((flagData) => { return createCountryTr(flagData) })}
                         </tbody>
                     </table>
                 </div>
+            </div>
+            
+            <div className="column">
+            
             </div>
         </div>
     );
