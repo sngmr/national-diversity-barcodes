98ca11a8d9ca1a7447f1a9c801cbce017b160081
Shingo Mori <shingo-mori@azapa.co.jp>
2021-06-14 20:26:53 +0900

    Change title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
             <section className="hero is-dark">
                 <div className="hero-body">
                     <div className="container">
-                        <h1 className="title">National Diversity Barcodes</h1>
+                        <h1 className="title">[WIP]National Diversity Barcodes</h1>
                     </div>
                 </div>
             </section>
